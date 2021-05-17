0ba7cce17dc9f69c09a2db980d92ae5817ba25ee
Vilius <polerisviliux@gmail.com>
2021-05-18 00:45:48 +0300

    Istaisyta klaida, fxml failas sekmingai susijungia su kodu

--- src/main/java/ui/MainController.java
+++ src/main/java/ui/MainController.java
@@ -1,13 +1,21 @@
-//package ui;
-//
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.input.MouseEvent;
-//
-//import java.io.IOException;
-//
-//public class MainController {
-//    public void onClickEvent(MouseEvent mouseEvent) throws IOException {
-//      //  FXMLLoader.load(getClass().getResource("islaidos.fxml"));
-//
-//    }
-//}
+package ui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainController {
+    public void onClickEvent(MouseEvent mouseEvent) throws IOException {
+        Parent outcome = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/islaidos.fxml")));
+        Scene scene = new Scene(outcome);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Islaidos");
+        stage.show();
+    }
+}
--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -1,37 +1,31 @@
-//package ui;
-//
-//
-//import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-//
-//import java.io.File;
-//import java.net.URL;
-//
-//public class UserInterface extends Application {
-//
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//       Parent root = null;
-//       URL url = null;
-//
-//       try {
-//           url = getClass().getResource("src/main/resources/start.fxml");
-//           root = FXMLLoader.load(url);
-//           System.out.println("fxmlResource: " + url.toString() + root.toString());
-//       } catch (Exception e){
-//           System.out.println("fxmlurl: " + url.toString());
-//           System.out.println("fxmlrootTO String" + root.toString());
-//       }
-//
-//
-//        stage.setTitle("Finansai");
-//        stage.show();
-//    }
-//
-//
-//
-//}
+package ui;
+
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.Objects;
+
+public class UserInterface extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+       Parent root = null;
+       try {
+           root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/start.fxml")));
+       } catch (Exception e){
+           e.printStackTrace();
+       }
+        assert root != null;
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Finansai");
+        stage.show();
+    }
+
+
+
+}
